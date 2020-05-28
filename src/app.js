f9313cc9770eb1a6004796c221412a1ee25cdacf
Mario <mario.saliba98@gmail.com>
2020-05-28 22:31:15 +0300

    adds a socket to the api

--- src/app.js
+++ src/app.js
@@ -8,17 +8,26 @@ dotenv.config();
 
 const app = express();
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+});
+
+setInterval(() => {
+  io.emit('ping', { data: (new Date()) / 1 });
+}, 2000);
+
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
 app.use(cors({ origin: true }));
 
-// app.options('*', cors());
-
 app.use('/auth', authRouter);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server run on port ${port}`);
 });
